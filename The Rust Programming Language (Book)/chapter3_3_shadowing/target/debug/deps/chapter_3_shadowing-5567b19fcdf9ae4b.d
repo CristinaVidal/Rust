a8fde5f589fdf4c0f0e1b062659b38f399b311ff
CristinaVidal <vidal.cristina14@gmail.com>
2020-11-07 22:09:20 +0100

    Test

---
+++ The Rust Programming Language (Book)/chapter3_3_shadowing/target/debug/deps/chapter_3_shadowing-5567b19fcdf9ae4b.d
@@ -0,0 +1,5 @@
+C:\Users\vidal\Desktop\Repositorios GIT\Rust\The Rust Programming Language (Book)\chapter3_3_shadowing\target\debug\deps\chapter_3_shadowing-5567b19fcdf9ae4b.rmeta: src\main.rs
+
+C:\Users\vidal\Desktop\Repositorios GIT\Rust\The Rust Programming Language (Book)\chapter3_3_shadowing\target\debug\deps\chapter_3_shadowing-5567b19fcdf9ae4b.d: src\main.rs
+
+src\main.rs:
